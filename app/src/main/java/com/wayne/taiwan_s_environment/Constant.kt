7621f12773acbd282012a214f36672654d88e567
LiuEnWei <w62501@gmail.com>
2020-06-24 01:44:00 +0800

    release key

--- app/src/main/java/com/wayne/taiwan_s_environment/Constant.kt
+++ app/src/main/java/com/wayne/taiwan_s_environment/Constant.kt
@@ -4,7 +4,7 @@ class Constant {
 
     companion object {
         const val EPA_DATA_URL = "https://data.epa.gov.tw/"
-        const val EPA_DATA_API_KEY = "9be7b239-557b-4c10-9775-78cadfc555e9" //TODO This key is test api key
+        const val EPA_DATA_API_KEY = "15d3267a-e830-485d-b0be-df3b3cde8c02"
         const val DB_NAME = "app.db"
         const val PREFS_NAME = "prefs"
     }
