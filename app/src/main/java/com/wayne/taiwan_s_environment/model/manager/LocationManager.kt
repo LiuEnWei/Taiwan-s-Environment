52604b9ae92f2981fa438fdb75fc0a02d3398348
LiuEnWei <w62501@gmail.com>
2020-07-12 10:34:37 +0800

    add LocationManager, modified Taiwan xml

---
+++ app/src/main/java/com/wayne/taiwan_s_environment/model/manager/LocationManager.kt
@@ -0,0 +1,108 @@
+package com.wayne.taiwan_s_environment.model.manager
+
+import android.Manifest
+import android.content.Context
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Looper
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import timber.log.Timber
+
+class LocationManager(val context: Context, var listener: LocationManagerListener?) : LifecycleObserver {
+
+    private var fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private var locationRequest: LocationRequest = LocationRequest.create().apply {
+        interval = 10000
+        fastestInterval = 5000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult ?: return
+            val location = locationResult.locations.first()
+            Timber.e("location : $location")
+            listener?.onLocationUpdated(location)
+        }
+    }
+
+    private var isLocationUpdates: Boolean = false
+
+    init {
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(context)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            Timber.e("locationSettingsResponse success")
+        }
+
+        task.addOnFailureListener { exception ->
+            Timber.e("locationSettingsResponse failure")
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+//                    exception.startResolutionForResult(requireActivity(), REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+    }
+
+    fun isLocationUpdate(): Boolean {
+        return isLocationUpdates
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun resume(){
+        startLocationUpdates()
+        Timber.e("resume()")
+    }
+
+    fun startLocationUpdates() {
+        if (isLocationUpdates) return
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                Timber.e("lastLocation : $location")
+                location?:return@addOnSuccessListener
+                listener?.onLocationUpdated(location)
+            }
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            isLocationUpdates = true
+        }
+        listener?.updateFloatingLocationButton()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun pause() {
+       stopLocationUpdates()
+        Timber.e("pause()")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun destroy() {
+        listener = null
+        Timber.e("destroy()")
+    }
+
+    fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        isLocationUpdates = false
+        listener?.updateFloatingLocationButton()
+    }
+
+}
